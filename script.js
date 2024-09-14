4d467ddcca9edc56d1b2093030c506b832b3de7a
Matheus Freitas <mgsoutodelira@gmail.com>
2024-09-14 13:51:37 -0300

    update foto

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function toggleMode() {
   html.classList.toggle('light');
   const img = document.querySelector('#profile img')
   if (html.classList.contains('light')) {
-    img.setAttribute("src", "./assets/red.jpg")
+    img.setAttribute("src", "./assets/Avatar1.png")
   } else {
-    img.setAttribute("src", "./assets/black.jpg");
+    img.setAttribute("src", "./assets/Avatar.png");
   }
 }
